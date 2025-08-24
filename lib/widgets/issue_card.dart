494d0b9cbaebcb7bb8006862dfeb1ca48a8ef403
Sarthak Patil <sarthak05patil@gmail.com>
2025-08-24 21:01:35 +0530

    Refactor: Display issue image in details dialog
    The issue image is now displayed in the issue details dialog.
    
    The following changes were made:
    - Imported `dart:convert` and `dart:io`.
    - Decoded the base64 encoded image string to `Uint8List`.
    - Displayed the image using `Image.memory` widget.

--- lib/widgets/issue_card.dart
+++ lib/widgets/issue_card.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../models/issue.dart';
@@ -520,6 +523,7 @@ class _IssueCardState extends State<IssueCard>
   }
 
   void _showIssueDetails() {
+    Uint8List? decodedBytes = base64Decode(widget.issue.photo!);
     showDialog(
       context: context,
       builder: (context) => Dialog(
@@ -568,6 +572,7 @@ class _IssueCardState extends State<IssueCard>
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       _buildDetailSection('Basic Information', [
+                        Image.memory(decodedBytes, fit: BoxFit.scaleDown),
                         _buildDetailRow('Ticket ID', widget.issue.ticketId),
                         _buildDetailRow('Title', widget.issue.title),
                         _buildDetailRow('Category', widget.issue.category),
